c626bf2ea0f5605df610282ab300fb81a0da6c97
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-04-19 10:16:27 +0530

    answer of question 3

---
+++ 1_introduction_to_groovy/introduction_to_groovy/src/main/groovy/q3/Q3Pattern.groovy
@@ -0,0 +1,14 @@
+package q3
+
+/**
+ * Created by gaurav on 18/4/17.
+ */
+class Q3Pattern {
+    static void main(String[] args) {
+        5.times { rows ->
+                rows.times { cols -> print "*" };
+                    println "";
+
+        }
+    }
+}
