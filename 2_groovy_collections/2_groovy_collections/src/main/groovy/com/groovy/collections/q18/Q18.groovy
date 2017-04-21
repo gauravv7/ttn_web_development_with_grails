6b6412fed0bf5477e91977593a4bf2c24288bbff
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-04-21 23:04:17 +0530

    answer of question 18

---
+++ 2_groovy_collections/2_groovy_collections/src/main/groovy/com/groovy/collections/q18/Q18.groovy
@@ -0,0 +1,11 @@
+package com.groovy.collections.q18
+
+/**
+ * Created by gaurav on 19/4/17.
+ */
+class Q18 {
+    public static void main(String[] args) {
+        println Map.class
+        println Map.getClass()
+    }
+}
