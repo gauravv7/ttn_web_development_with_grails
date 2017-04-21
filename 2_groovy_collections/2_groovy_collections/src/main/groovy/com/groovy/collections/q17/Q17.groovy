915226f60fbe9c9687a2f7d2840d8c9dea6a366e
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-04-21 23:04:10 +0530

    answer of question 17

---
+++ 2_groovy_collections/2_groovy_collections/src/main/groovy/com/groovy/collections/q17/Q17.groovy
@@ -0,0 +1,15 @@
+package com.groovy.collections.q17
+
+/**
+ * Created by gaurav on 19/4/17.
+ */
+class Q17 {
+    public static void main(String[] args) {
+        Map m1 = ['1': 1, '2': 2]
+        Map m2 = ['3': 3, '4': 4]
+        println m1
+        println m2
+        println "m1+m2="+m1+m2
+
+    }
+}
