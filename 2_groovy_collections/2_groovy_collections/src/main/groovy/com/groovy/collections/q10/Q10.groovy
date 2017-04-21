14793041de246943b25ac884863c0263de6640ca
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-04-21 23:03:06 +0530

    answer of question 10

---
+++ 2_groovy_collections/2_groovy_collections/src/main/groovy/com/groovy/collections/q10/Q10.groovy
@@ -0,0 +1,14 @@
+package com.groovy.collections.q10
+
+/**
+ * Created by gaurav on 19/4/17.
+ */
+class Q10 {
+    public static void main(String[] args) {
+        Range range = 3..15
+        println "first element: " +range.from
+        println "second element: " +range.get(2)
+        println "Last element: " + range.to
+
+    }
+}
