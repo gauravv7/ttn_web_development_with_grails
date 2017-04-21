c313d97253b061e7832657eda32754f219e5fbca
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-04-21 23:36:10 +0530

    added project

---
+++ 3_spock_unit_testing/src/com/im/test/EmailService.groovy
@@ -0,0 +1,14 @@
+package com.im.test
+
+class EmailService {
+    void sendCancellationEmail(User user, String content){
+               //sends cancellation email
+    }
+
+    void sendPasswordResetEmail(User user, String content){
+               //sends password reset email
+    }
+    void sendPasswordResetFailureEmail(User user, String content){
+               //sends password reset email
+    }
+}
