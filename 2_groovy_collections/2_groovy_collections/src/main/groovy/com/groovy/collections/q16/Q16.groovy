ef6220eb6c37d3e752e87e0da2a0d836d680088d
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-04-21 23:04:01 +0530

    answer of question 16

---
+++ 2_groovy_collections/2_groovy_collections/src/main/groovy/com/groovy/collections/q16/Q16.groovy
@@ -0,0 +1,12 @@
+package com.groovy.collections.q16
+
+/**
+ * Created by gaurav on 19/4/17.
+ */
+class Q16 {
+    public static void main(String[] args) {
+        Map<String, Integer> m = new TreeMap<>();
+        (1..10).each{ m.put("frnd${it}", it+10) }
+        println m
+    }
+}
