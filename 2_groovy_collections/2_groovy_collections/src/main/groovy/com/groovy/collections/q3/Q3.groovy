c9e35120336d388ff4b6cd887407bf240d711424
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-04-21 23:00:38 +0530

    answer of question 3

---
+++ 2_groovy_collections/2_groovy_collections/src/main/groovy/com/groovy/collections/q3/Q3.groovy
@@ -0,0 +1,17 @@
+package com.groovy.collections.q3
+
+/**
+ * Created by gaurav on 19/4/17.
+ */
+class Q3 {
+
+    public static void main(String[] args) {
+
+
+        List list = [11, 12, 13, 14]
+        List list1 = [13, 14, 15]
+
+        println(" list - list1 : ${list-list1}")
+    }
+
+}
