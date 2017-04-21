4d0570dab60f846741fc677ccee934eed7e3118a
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-04-21 23:00:07 +0530

    answer of question 1

---
+++ 2_groovy_collections/2_groovy_collections/src/main/groovy/com/groovy/collections/q1/Q1.groovy
@@ -0,0 +1,15 @@
+package com.groovy.collections.q1
+
+/**
+ * Created by gaurav on 19/4/17.
+ */
+class Q1 {
+    public static void main(String[] args) {
+        List l = new ArrayList<>();
+        l[11] = "myElement"
+        println "l[11]=${l[11]}"
+        println "l.get(5)=${l.get(5)}"
+        println "l=${l}"
+
+    }
+}
