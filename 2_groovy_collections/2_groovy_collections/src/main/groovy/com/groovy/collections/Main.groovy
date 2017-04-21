ed054853d144f05482f6b74414d689f93de3f76b
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-04-21 23:19:14 +0530

    added main class for project

---
+++ 2_groovy_collections/2_groovy_collections/src/main/groovy/com/groovy/collections/Main.groovy
@@ -0,0 +1,129 @@
+package com.groovy.collections
+
+import com.groovy.collections.q1.Q1
+import com.groovy.collections.q10.Q10
+import com.groovy.collections.q11.Q11
+import com.groovy.collections.q12.Q12
+import com.groovy.collections.q13.Q13
+import com.groovy.collections.q14.Q14
+import com.groovy.collections.q15.Q15
+import com.groovy.collections.q16.Q16
+import com.groovy.collections.q17.Q17
+import com.groovy.collections.q18.Q18
+import com.groovy.collections.q19.Q19
+import com.groovy.collections.q2.Q2
+import com.groovy.collections.q20.Q20
+import com.groovy.collections.q21.Q21
+import com.groovy.collections.q22.Q22
+import com.groovy.collections.q23.Q23
+import com.groovy.collections.q3.Q3
+import com.groovy.collections.q4.Q4
+import com.groovy.collections.q5.Q5
+import com.groovy.collections.q6.Q6
+import com.groovy.collections.q7.Q7
+import com.groovy.collections.q8.Q8
+import com.groovy.collections.q9.Q9
+
+/**
+ * Created by gaurav on 21/4/17.
+ */
+class Main {
+    public static void main(String[] args) {
+        switch (args[0]) {
+            case "q1":
+                Q1.Main.main(args);
+                break
+
+            case "q2":
+                Q2.Main.main(args);
+                break;
+
+            case "q3":
+                Q3.main(args);
+                break;
+
+            case "q4":
+                Q4.Main.main(args);
+                break;
+
+            case "q5":
+                Q5.Main.main(args);
+                break;
+
+            case "q6":
+                Q6.Main.main(args);
+                break;
+
+            case "q7":
+                Q7.main(args);
+                break;
+
+            case "q8":
+                Q8.main(args);
+                break;
+
+            case "q9":
+                Q9.main(args);
+                break;
+
+            case "q10":
+                Q10.main(args);
+                break;
+
+            case "q11":
+                Q11.main(args);
+                break;
+
+            case "q12":
+                Q12.main(args);
+                break;
+
+            case "q13":
+                Q13.main(args);
+                break;
+
+            case "Q14":
+                Q14.main(args);
+                break;
+
+            case "q15":
+                Q15.main(args);
+                break;
+
+            case "q16":
+                Q16.main(args);
+                break;
+
+            case "q17":
+                Q17.main(args);
+                break;
+
+            case "q18":
+                Q18.main(args);
+                break;
+
+            case "q19":
+                Q19.main(args);
+                break;
+
+            case "q20":
+                Q20.main(args);
+                break;
+
+            case "q21":
+                Q21.main(args);
+                break;
+
+            case "q22":
+                Q22.main(args);
+                break;
+
+            case "q23":
+                Q23.main(args);
+                break;
+
+            default:
+                System.out.println("invalid input, please specify as q<(1-23)>");
+        }
+    }
+}
