77bb58ca1972eb3a74ffc2fe56a56efeecac4ca3
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-04-21 23:01:30 +0530

    answer of question 7

---
+++ 2_groovy_collections/2_groovy_collections/src/main/groovy/com/groovy/collections/q7/Q7.groovy
@@ -0,0 +1,16 @@
+package com.groovy.collections.q7
+
+/**
+ * Created by gaurav on 19/4/17.
+ */
+class Q7 {
+
+
+    public static void main(String[] args) {
+
+        List list = [14,12, 11,10, 16, 15,12, 10, 99, 90, 14, 16, 35]
+
+        println( list.sort().reverse().unique())
+
+    }
+}
