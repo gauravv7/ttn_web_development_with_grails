997184525433e01a6f56e4749fd81c1e02d9ae1c
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-04-21 23:04:23 +0530

    answer of question 19

---
+++ 2_groovy_collections/2_groovy_collections/src/main/groovy/com/groovy/collections/q19/Q19.groovy
@@ -0,0 +1,14 @@
+package com.groovy.collections.q19
+
+/**
+ * Created by gaurav on 19/4/17.
+ */
+class Q19 {
+    public static void main(String[] args) {
+        Map m = ['1' : 2, '2' : 3, '3' : 4, '2':5]
+        println "Trying to make a Map m = ['1' : 2, '2' : 3, '3' : 4, '2':5]"
+        println "yes it is a valid construction"
+        println m
+        println "m['2']=${m['2']}"
+    }
+}
