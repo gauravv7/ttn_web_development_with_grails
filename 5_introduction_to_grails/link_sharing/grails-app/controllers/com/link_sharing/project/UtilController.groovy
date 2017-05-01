b837e32a2d8e75a841a012b08eba8aa4dbd8e760
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-05-01 05:52:05 +0530

    question 9:

---
+++ 5_introduction_to_grails/link_sharing/grails-app/controllers/com/link_sharing/project/UtilController.groovy
@@ -0,0 +1,14 @@
+package com.link_sharing.project
+
+/**
+ * Created by gaurav on 1/5/17.
+ */
+class UtilController {
+    def index() {
+
+        log.info("Success")
+        log.warn("warning..")
+        log.error("error....")
+
+    }
+}
