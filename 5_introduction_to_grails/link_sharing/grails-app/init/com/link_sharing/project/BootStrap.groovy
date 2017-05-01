79d73a8ffa75f74dd7106c2f62c2f72f8b265b17
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-05-01 06:38:04 +0530

    question 13

--- 5_introduction_to_grails/link_sharing/grails-app/init/com/link_sharing/project/BootStrap.groovy
+++ 5_introduction_to_grails/link_sharing/grails-app/init/com/link_sharing/project/BootStrap.groovy
@@ -1,8 +1,19 @@
 package com.link_sharing.project
 
+import grails.config.Config
+
 class BootStrap {
 
+    def grailsApplication
+
     def init = { servletContext ->
+        // Get configuration from GrailsApplication.
+        final Config configuration = grailsApplication.config
+
+        // Get the value for sample.config.
+        final String sampleConfigValue = configuration.getProperty('grails.app.context')
+
+        log.info("Value for sample.config configuration property = $sampleConfigValue")
     }
     def destroy = {
     }
