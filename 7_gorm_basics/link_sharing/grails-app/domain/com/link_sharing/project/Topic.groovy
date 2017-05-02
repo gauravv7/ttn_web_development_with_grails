231f9c7eb59fe6314128129764e94c4b9fe5d5e4
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-05-03 01:15:13 +0530

    question 8-11

--- 7_gorm_basics/link_sharing/grails-app/domain/com/link_sharing/project/Topic.groovy
+++ 7_gorm_basics/link_sharing/grails-app/domain/com/link_sharing/project/Topic.groovy
@@ -3,6 +3,7 @@ package com.link_sharing.project
 import com.link_sharing.project.User as User
 import com.link_sharing.project.Subscription as Subscription
 import com.link_sharing.project.Resource as Resource
+import com.link_sharing.project.constants.Constants
 
 class Topic {
 
@@ -26,5 +27,19 @@ class Topic {
 
     String toString() {
         return "${topicName}, ${createdBy}"
+    }
+
+    def afterInsert() {
+        Topic.withNewSession {
+            Subscription subscription = new Subscription(
+                    createdBy: this.createdBy,
+                    topic: this,
+                    seriousness: Constants.SERIOUSNESS)
+            if (subscription.validate() && subscription.save(flush: true)) {
+                log.info "${subscription}-> ${this.createdBy} subscribed for ${this}"
+            } else {
+                log.error "Subscription failed: ${subscription.errors.allErrors}"
+            }
+        }
     }
 }
--- 7_gorm_basics/link_sharing/src/main/groovy/com/link_sharing/project/constants/Constants.groovy
+++ 7_gorm_basics/link_sharing/src/main/groovy/com/link_sharing/project/constants/Constants.groovy
@@ -1,9 +1,12 @@
 package com.link_sharing.project.constants
 
+import com.link_sharing.project.Seriousness
+
 /**
  * Created by gaurav on 2/5/17.
  */
 class Constants {
     static final String PASSWORD_NORMAL = "123456"
     static final String PASSWORD_ADMIN  = "admin123456"
+    static final Seriousness SERIOUSNESS = Seriousness.VERY_SERIOUS
 }
