9a9f2751d9057b5183530300a556e5fcf25a8389
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-05-02 23:05:34 +0530

    question 1: createUsers()

--- 7_gorm_basics/link_sharing/grails-app/init/com/link_sharing/project/BootStrap.groovy
+++ 7_gorm_basics/link_sharing/grails-app/init/com/link_sharing/project/BootStrap.groovy
@@ -14,7 +14,19 @@ class BootStrap {
         final String sampleConfigValue = configuration.getProperty('grails.app.context')
 
         log.info("Value for sample.config configuration property = $sampleConfigValue")
+
+        createUser()
     }
     def destroy = {
+    }
+
+    void createUser() {
+
+
+        User normalUser = new User(userName: "user", firstName: "normal", lastName: "normalLastName", email: "user@ttn.com",
+                password: "normal1", admin: false, active: false)
+        User adminUser = new User(userName: "admin", firstName: "admin", lastName: "adminLastName", email: "admin@ttnd.com",
+                password: "admin1", admin: true,active: true)
+
     }
 }
