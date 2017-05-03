7ae66417fe697a98f38b24f060ca5f9884ff9e80
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-05-03 12:27:50 +0530

    question 13: closures used

--- 2_groovy_collections/2_groovy_collections/src/main/groovy/com/groovy/collections/q13/Q13.groovy
+++ 2_groovy_collections/2_groovy_collections/src/main/groovy/com/groovy/collections/q13/Q13.groovy
@@ -5,13 +5,12 @@ package com.groovy.collections.q13
  */
 class Q13 {
     public static void main(String[] args) {
-        String s = "abcdeaaaabbbbcccccdd"
-        println getOccurences(s, 'a'.charAt(0))
-    }
+        Closure getOccurences = { s1,c ->
+            def counter=0;
+            s1.each { chrctr -> if(chrctr.charAt(0)==c) counter++ }
+            return counter;
+        }
 
-    public static int getOccurences(String s, Character c){
-        int counter=0;
-        s.each { chrctr -> if(chrctr.charAt(0)==c) counter++ }
-        return counter;
+        println getOccurences("abcdeaaaabbbbcccccdd", 'a'.charAt(0))
     }
 }
