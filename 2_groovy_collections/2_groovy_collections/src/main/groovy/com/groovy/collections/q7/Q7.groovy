38d1010cf861382fca03eb135211d70bec93b13c
Gaurav Arora <gaurav.arora0403@gmail.com>
2017-05-03 10:36:30 +0530

    question 7: removing duplicate elements 1st and then sorting for optimal way

--- 2_groovy_collections/2_groovy_collections/src/main/groovy/com/groovy/collections/q7/Q7.groovy
+++ 2_groovy_collections/2_groovy_collections/src/main/groovy/com/groovy/collections/q7/Q7.groovy
@@ -10,7 +10,7 @@ class Q7 {
 
         List list = [14,12, 11,10, 16, 15,12, 10, 99, 90, 14, 16, 35]
 
-        println( list.sort().reverse().unique())
+        println( list.unique().sort().reverse())
 
     }
 }
